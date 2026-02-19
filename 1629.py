d362886828a23b53ff1d2f73447c87695fd85b2d
jhkim000601@naver.com <jhkim000601@naver.com>
2026-02-19 16:38:30 +0900

    fix: 시간초과 수정

--- 1629.py
+++ 1629.py
@@ -1,2 +1,12 @@
-A, B, C = map(int, input("A, B, C  입력: ").split(" "))
-print(pow(A, B, C))
+def cal(a, b, c):
+    if b == 1:
+        return a % c
+    else:
+        k = cal(a, b//2, c)
+        if b % 2 == 0: # b가 짝수일때
+            return (k*k)%c
+        else: # b가 홀수일때
+            return (k*k*a)%c
+        
+a, b, c = map(int, input().split(" "))
+print(cal(a, b, c))
